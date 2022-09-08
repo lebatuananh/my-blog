91d1667a8aaff65ae7d7b6584aaf8eb0379df50b
codefactor-io <support@codefactor.io>
2022-09-08 01:30:01 +0000

    [CodeFactor] Apply fixes

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,53 +1,53 @@
 const siteMetadata = {
-  title: 'Blog | MRA',
-  author: 'MRA',
-  headerTitle: 'Mere Musings',
-  description: 'My personal blog where I share my musings',
-  snippets: 'Reuseable code snippets collected by Parth',
-  language: 'en-us',
-  theme: 'system', // system, dark or light
-  siteUrl: 'https://musing.vercel.app',
-  siteRepo: 'https://github.com/mramra3004/my-blog',
-  siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
-  email: 'tuananh300496@gmail.com',
-  github: 'https://github.com/mramra3004',
-  twitter: 'https://twitter.com/lone_Musk',
-  linkedin: 'https://www.linkedin.com/in/parth-desai-2bb1b0160/',
-  website: 'https://parthdesai.vercel.app',
-  locale: 'en-US',
+  title: "Blog | MRA",
+  author: "MRA",
+  headerTitle: "Mere Musings",
+  description: "My personal blog where I share my musings",
+  snippets: "Reuseable code snippets collected by Parth",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "https://musing.vercel.app",
+  siteRepo: "https://github.com/mramra3004/my-blog",
+  siteLogo: "/static/images/logo.png",
+  image: "/static/images/avatar.png",
+  socialBanner: "/static/images/twitter-card.png",
+  email: "tuananh300496@gmail.com",
+  github: "https://github.com/mramra3004",
+  twitter: "https://twitter.com/lone_Musk",
+  linkedin: "https://www.linkedin.com/in/parth-desai-2bb1b0160/",
+  website: "https://parthdesai.vercel.app",
+  locale: "en-US",
   analytics: {
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: "", // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-F6V2QTJ628', // e.g. UA-000000-2 or G-XXXXXXX
+    umamiWebsiteId: "", // e.g. 123e4567-e89b-12d3-a456-426614174000
+    googleAnalyticsId: "G-F6V2QTJ628", // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
-    provider: 'emailOctopus',
+    provider: "emailOctopus",
   },
   comment: {
-    provider: 'giscus',
+    provider: "giscus",
     giscusConfig: {
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname',
-      reactions: '1',
-      metadata: '0',
+      mapping: "pathname",
+      reactions: "1",
+      metadata: "0",
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
-      inputPosition: 'bottom',
-      lang: 'en',
-      darkTheme: 'dark',
-      themeURL: '',
+      theme: "light",
+      inputPosition: "bottom",
+      lang: "en",
+      darkTheme: "dark",
+      themeURL: "",
     },
   },
   socialAccount: {
-    twitter: 'lone_Musk',
+    twitter: "lone_Musk",
   },
-}
+};
 
-module.exports = siteMetadata
+module.exports = siteMetadata;
